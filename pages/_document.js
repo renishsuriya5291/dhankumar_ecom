4e1c119598ee068747369fd79d14dfcae0ad4e3c
RENISH SURIYA <renishsuriya1441@gmail.com>
2024-04-07 23:15:55 +0530

    msg

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {process.env.NEXT_PWA_STATUS === 1 ||
-            (process.env.NEXT_PWA_STATUS === "1" && (
-              <link rel="manifest" href="/manifest.json" />
-            ))}
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
