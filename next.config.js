50b3d3558913675437289ba0593d4c9e435e4253
RENISH SURIYA <renishsuriya1441@gmail.com>
2024-04-07 22:40:23 +0530

    msg

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
-/** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa');
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [`${hostname}`]
+    domains: [hostname] // Using hostname directly instead of template string
   },
-}
-const pwa = process.env.NEXT_PWA_STATUS;
-const nextConfigWithPwa = withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+};
 
+const pwa = process.env.NEXT_PWA_STATUS === '1'; // Checking if PWA is enabled
+
+// Wrapping the nextConfig with PWA configuration only if PWA is enabled
+const nextConfigWithPwa = pwa ? withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+  },
+}) (nextConfig) : nextConfig;
+
+module.exports = nextConfigWithPwa;
